f3d777d77eb1859dd518b1564e35f3b648874fca
ujiuji1259 <suzzz428@gmail.com>
2021-04-29 00:52:09 +0900

    add document of fuzzy matcher

--- src/disease_normalizer/converter/fuzzy_matcher.py
+++ src/disease_normalizer/converter/fuzzy_matcher.py
@@ -1,3 +1,8 @@
+"""Converter using fuzzy match.
+
+Fuzzy match by simstring. We use the cosine similarity of character-level bigrams.
+"""
+
 import os
 from pathlib import Path
 
@@ -10,6 +15,16 @@ from .. import utils
 
 
 class FuzzyMatchConverter(object):
+    """Fuzzy Matcher
+
+    args:
+        dictionary: List[DictEntry]
+
+    attributes:
+        dict Dict[str: DictEntry]: dict of manbyo dictionary
+        db: DictDatabase of simstring
+        searcher: searcher of simstring
+    """
     def __init__(self, dictionary):
         self.dict = {d.name: d for d in dictionary}
         self.db = DictDatabase(CharacterNgramFeatureExtractor(2))
@@ -17,6 +32,15 @@ class FuzzyMatchConverter(object):
         self.searcher = Searcher(self.db, CosineMeasure())
 
     def convert(self, word, alpha=0.5):
+        """Convert word to normalized form.
+
+        Args:
+            word str: surface form of the disease that you want to normalize
+            alpha float: minimum value of cosine similarity
+
+        Return:
+            DictEntry: DictEntry of normalized disease
+        """
         results = self.searcher.ranked_search(word, alpha)
         if len(results) != 0:
             # results = [(sim, word), ...]
