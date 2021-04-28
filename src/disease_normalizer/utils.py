a02ddc60652fc70d5a3babc57d7d10eff7727724
ujiuji1259 <suzzz428@gmail.com>
2021-04-29 00:44:18 +0900

    add document in utils.py

--- src/disease_normalizer/utils.py
+++ src/disease_normalizer/utils.py
@@ -1,14 +1,33 @@
+"""Set of utility functions
+"""
+
 import csv
 from dataclasses import dataclass
 
 @dataclass
 class DictEntry:
+    """Entry of manbyo dictionary
+
+    attributes:
+        name: surface form of the disease
+        icd: icd-10 code
+        norm: normalized form of the disease
+        level: confidence level in the manbyo dictionary
+    """
     name: str
     icd: str
     norm: str
     level: str
 
 def load_dict(path):
+    """Load manbyo dictionary
+
+    args:
+        path: str
+
+    return:
+        data: List[DictEntry]
+    """
     data = []
     with open(path, 'r') as f:
         reader = csv.reader(f)
