41b1baeadeec151fb6e30e4e3eb5fef11001d24a
ujiuji1259 <suzzz428@gmail.com>
2021-04-29 00:56:29 +0900

    add document of exact matcher

--- src/disease_normalizer/converter/exact_matcher.py
+++ src/disease_normalizer/converter/exact_matcher.py
@@ -1,9 +1,29 @@
+"""Converter using exact match
+
+Convert disease using exact match.
+"""
 import csv
 from .. import utils
 
 class ExactMatchConverter(object):
+    """ExactMatcher
+
+    Args:
+        dictionary List[DictEntry]: manbyo dictionary
+
+    Attributes:
+        dict Dict[str: DictEntry]: manbyo dictionary
+    """
     def __init__(self, dictionary):
         self.dict = {d.name: d for d in dictionary}
 
     def convert(self, word):
+        """Convert surface form of the dictionary into the normalized form
+
+        Args:
+            word str: surface form of the disease
+
+        Returns:
+            DictEntry: normalized form of the input disease.
+        """
         return self.dict.get(word, utils.DictEntry(None, None, None, None))
