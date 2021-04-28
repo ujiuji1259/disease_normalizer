8207c52bb18b09af267301dbf04e46d42cf921ba
ujiuji1259 <suzzz428@gmail.com>
2021-04-29 01:50:12 +0900

    add document of dnorm

--- src/disease_normalizer/converter/dnorm/dnorm_converter.py
+++ src/disease_normalizer/converter/dnorm/dnorm_converter.py
@@ -1,3 +1,7 @@
+"""Fuzzy match using DNorm
+
+DNorm is the disease normalization model based on the ranking model with tf-idf vector.
+"""
 import os
 from pathlib import Path
 
@@ -10,15 +14,42 @@ BASE_URL = "http://aoi.naist.jp/DNorm/dnorm.pkl"
 
 
 class DNormConverter(object):
+    """DNorm converter
+
+    Args:
+        dictionary List[DictEntry]: manbyo dictionary
+
+    Attributes:
+        dict Dict[str: DictEntry]: manbyo dictionary
+        model DNorm: DNorm model
+    """
     def __init__(self, dictionary):
         self.dict = {d.name: d for d in dictionary}
         self.build_model(dictionary)
 
     def convert(self, word):
+        """Convert surface form of the disease into the normalized form.
+
+        Args:
+            word str: surface form of the disease
+
+        Returns:
+            DictEntry: DictEntry of normalized form of the disease
+        """
         result = self.model.predict([word], k=1)[0][0]
         return self.dict[result]
 
     def build_model(self, dictionary):
+        """Build dnorm
+
+        Load dnorm model from ~/.cache/Dnorm/dnorm.pkl if the path exists.
+        Otherwise, automatically download the dnorm file from remote-url.
+        You can specify the cache directory by setting the environment variable "DEFAULT_CACHE_PATH"
+
+        Args:
+            dictionary List[DictEntry]: manbyo dictionary
+
+        """
         DEFAULT_CACHE_PATH = os.getenv("DEFAULT_CACHE_PATH", "~/.cache")
         DEFAULT_DNORM_PATH = Path(os.path.expanduser(
                 os.path.join(DEFAULT_CACHE_PATH, "Dnorm")
