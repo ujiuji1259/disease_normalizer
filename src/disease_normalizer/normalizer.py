dc68e7eb987a88fde8e05dce165f6b924d7e679e
ujiuji1259 <suzzz428@gmail.com>
2021-04-29 15:34:28 +0900

    add converter in normalizer

--- src/disease_normalizer/normalizer.py
+++ src/disease_normalizer/normalizer.py
@@ -1,7 +1,39 @@
+import os
+
+from . import utils
+from .converter import dnorm, exact_matcher, fuzzy_matcher
+from .base_converter import BaseConverter
+
+BASE_URL = "http://aoi.naist.jp/norm/MANBYO_SABC.csv"
 
 class Normalizer(object):
     def __init__(self, preprocessor, converter):
-        pass
+        self.load_manbyo_dict()
+
+        if isinstance(converter, str):
+            if converter == "exact":
+                self.converter = exact_matcher.ExactMatchConverter(self.manbyo_dict)
+            elif converter == "fuzzy":
+                self.converter = fuzzy_matcher.FuzzyMatchConverter(self.manbyo_dict)
+            else:
+                self.converter = dnorm.dnorm_converter.DNormConverter(self.manbyo_dict)
+        elif isinstance(converter, BaseConverter):
+            self.converter = converter
+        else:
+            assert NotImplementedError, "Please specify converter by selecting (exact|fuzzy|dnorm) or creating your own converter inheriting BaseConverter"
+
+    def load_manbyo_dict(self):
+        DEFAULT_CACHE_PATH = os.getenv("DEFAULT_CACHE_PATH", "~/.cache")
+        DEFAULT_MANBYO_PATH = Path(os.path.expanduser(
+                        os.path.join(DEFAULT_CACHE_PATH, "norm")
+                ))
+        DEFAULT_MANBYO_PATH.mkdir(parents=True, exist_ok=True)
+
+        if not (DEFAULT_MANBYO_PATH / "MANBYO_SABC.csv").exists():
+            utils.download_fileobj(BASE_URL, DEFAULT_MANBYO_PATH / "MANBYO_SABC.csv")
+
+        self.manbyo_dict = utils.load_dict(DEFAULT_MANBYO_PATH / "MANBYO_SABC.csv")
+
 
     def normalize(self, words):
         pass
